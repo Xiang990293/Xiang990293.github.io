b6c9cdcb8da1e58c6837b9891f0b0baab91abfe0
Xiang990293 <990293kwi@gmail.com>
2022-04-22 19:43:37 +0800

    dabug

--- js/content_finder.js
+++ js/content_finder.js
@@ -1,5 +1,6 @@
 ﻿var path = document.location.pathname;
-const filename = path.substr(1,a.length-6);
+const filename = path.substr(1,path.length-6);
 
 var data = readTextFile(`./web_content/${file}.txt`);
 console.log(data);
+console.log(1);
