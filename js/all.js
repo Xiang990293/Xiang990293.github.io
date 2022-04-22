128ea96ea0727e76fbdae3a3e5ecbbc04e1fa8d8
Xiang990293 <990293kwi@gmail.com>
2022-04-22 10:59:09 +0800

    add new module

--- js/all.js
+++ js/all.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 var headerPage = document.getElementById("headerPage");
 headerPage.innerHTML = `
 <div id="head">
@@ -18,15 +20,12 @@ headerPage.innerHTML = `
 </div>
 `;
 
-
-
 var siderPage = document.getElementById("siderPage");
 siderPage.innerHTML = `<ul id="側導航欄"></ul>`;
 var ul_in_siderPage = siderPage.getElementsByTagName("ul");
 var content = document.getElementById("content");
 var h3_in_content = content.getElementsByTagName("h3");
 var do_count = content.getElementsByTagName("h3").length;
-
 
 for(var i = 0; i < do_count; i++){
     ul_in_siderPage[0].innerHTML = ul_in_siderPage[0].innerHTML + `
---
+++ js/content_finder.js
@@ -0,0 +1,11 @@
+﻿var path = document.location.pathname;
+const filename = path.substr(1,a.length-6);
+function contentFinder(file){
+    fs.readFile(`./web_content/${file}.txt`, (err,data) => {
+        if(err){
+            data = '';
+        }else{
+            
+        }
+    });
+}
