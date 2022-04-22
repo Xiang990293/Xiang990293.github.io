f75871f21c7e559add89a8393b70188bfb60ccbd
Xiang990293 <990293kwi@gmail.com>
2022-04-22 20:40:53 +0800

    content will appear when onload

--- js/content_finder.js
+++ js/content_finder.js
@@ -1,6 +1,8 @@
-﻿var path = document.location.pathname;
-const filename = path.substr(1,path.length-6);
+﻿function content_finder(file){
+    var path = document.location.pathname;
+    const filename = path.substr(1,path.length-6);
 
-var data = readTextFile(`./web_content/${file}.txt`);
-console.log(data);
-console.log(1);
+    var data = blob.text(`./web_content/${file}.txt`);
+    console.log(data);
+    console.log(1);
+}
