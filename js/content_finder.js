e33b7dc13c86ecce58c03a8658f01647c392d522
Xiang990293 <990293kwi@gmail.com>
2022-04-22 18:59:39 +0800

    delete the fs

--- js/content_finder.js
+++ js/content_finder.js
@@ -1,14 +1,5 @@
-﻿function content_finder(file){
+﻿var path = document.location.pathname;
+const filename = path.substr(1,a.length-6);
 
-    var path = document.location.pathname;
-    const filename = path.substr(1,a.length-6);
-
-    readTextFile(`./web_content/${file}.txt`);
-    fs.readFile(`./web_content/${file}.txt`, (err,data) => {
-        if(err){
-            data = '';
-        }else{
-            console.log(data);
-        }
-    });
-}
+var data = readTextFile(`./web_content/${file}.txt`);
+console.log(data);
