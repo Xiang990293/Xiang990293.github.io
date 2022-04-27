6f880a6a9df66d52ae1173dd39527bd3701305cc
Xiang990293 <990293kwi@gmail.com>
2022-04-27 18:49:24 +0800

    test-try-3

--- js/all.js
+++ js/all.js
@@ -18,23 +18,6 @@ headerPage.innerHTML = `
 </div>
 `;
 
-var content = content_finder();
-
-var siderPage = document.getElementById("siderPage");
-siderPage.innerHTML = `<ul id="側導航欄"></ul>`;
-var ul_in_siderPage = siderPage.getElementsByTagName("ul");
-var content = document.getElementById("content");
-var h3_in_content = content.getElementsByTagName("h3");
-var do_count = content.getElementsByTagName("h3").length;
-
-for(var i = 0; i < do_count; i++){
-    ul_in_siderPage[0].innerHTML = ul_in_siderPage[0].innerHTML + `
-    <li>
-        <a harf="">${h3_in_content[i].innerHTML}</a>
-    </li>
-    `;
-}
-
 var footerPage = document.getElementById("footerPage");
 footerPage.innerHTML = `
 <span>
--- js/content_finder.js
+++ js/content_finder.js
@@ -14,5 +14,20 @@
     const file = `web_constent/${textname}.txt`;
     console.log(file);
 
-    document.getElementById("content").innerHTML = new text(file);
+    document.getElementById("content").innerHTML = new text(file)
+    var siderPage = document.getElementById("siderPage");
+    siderPage.innerHTML = `<ul id="側導航欄"></ul>`;
+    var ul_in_siderPage = siderPage.getElementsByTagName("ul");
+    var content = document.getElementById("content");
+    var h3_in_content = content.getElementsByTagName("h3");
+    var do_count = content.getElementsByTagName("h3").length;
+
+    for(var i = 0; i < do_count; i++){
+        ul_in_siderPage[0].innerHTML = ul_in_siderPage[0].innerHTML + `
+        <li>
+            <a harf="">${h3_in_content[i].innerHTML}</a>
+        </li>
+        `;
+    }
 }
+
