439726a1d80a0bd640f8681bd3c129091b5ff89f
Xiang990293 <60068728+Xiang990293@users.noreply.github.com>
2023-07-19 01:22:26 +0800

    test 2187, add new web

--- js/content_finder.js
+++ js/content_finder.js
@@ -53,17 +53,24 @@ function content_finder(){
     //:new version
     returnText = returnText.replace(/```(.*?)```/gs,`<object title="1" style="background-color:gray; border: 3px solid black; border-radius: 10px; width:300px">$1</object>`)
     var titles = returnText.match(/^>+(.+?)$/gm)
-    for(title_index=0; title_index<titles.length; title_index++){
-        current_title_regexp = new RegExp(`^(${titles[title_index]})$`,"gm")
-        title_type_find = new RegExp(`^>+`,"gm")
-        title_type = titles[title_index].match(title_type_find)[0].length+1;
-        group_1_in_current_title = titles[title_index].replace(/^>+(.+?)$/gm,"$1")
-        returnText = returnText.replace(current_title_regexp,`<h${title_type} class="from_txt" id="${group_1_in_current_title}-${title_index}">${group_1_in_current_title}</h${title_type}>`)
-    }
-    returnText = returnText.replace(/^----$/gm, "<hr/>");
-    returnText = returnText.replace(/^(?<!<)(.+?)(?!>)$/gm, "<p>$1</p>");
 
-    document.getElementById("content_text").innerHTML = returnText;
+    for(title_index=0; title_index<titles.length; title_index++){
+		
+		temp_title = titles[title_index].replace(/(\\)/gm,"\\$1").replace(/(\()/gm,"\\$1").replace(/(\))/gm,"\\$1").replace(/(\!)/gm,"\\$1").replace(/(\^)/gm,"\\$1").replace(/(\$)/gm,"\\$1").replace(/(\*)/gm,"\\$1").replace(/(\+)/gm,"\\$1").replace(/(\?)/gm,"\\$1").replace(/(\{)/gm,"\\$1").replace(/(\})/gm,"\\$1").replace(/(\[)/gm,"\\$1").replace(/(\])/gm,"\\$1").replace(/(\,)/gm,"\\$1").replace(/(\.)/gm,"\\$1").replace(/(\:)/gm,"\\$1").replace(/(\<)/gm,"\\$1").replace(/(\=)/gm,"\\$1").replace(/(\|)/gm,"\\$1").replace(/(\-)/gm,"\\$1") //.replace(/(\>)/gm,"\\$1")
+        current_title_regexp = new RegExp(`^(${temp_title})$`,"gm")
+        title_type = temp_title.match(/^>+/gm)[0].length+1;
+		
+		group_1_in_current_title = titles[title_index].replace(/^>+(.+?)$/gm,"$1")
+        
+		returnText = returnText.replace(current_title_regexp,`<h${title_type} class="from_txt" id="${group_1_in_current_title}-${title_index}">${group_1_in_current_title}</h${title_type}>`)
+    }
+    
+	returnText = returnText.replace(/^----$/gm, "<hr/>")
+	returnText = returnText.replace(/^(?<!<)(.+?)(?!>)$/gm, "<p>$1</p>")
+
+
+	
+	document.getElementById("content_text").innerHTML = returnText
     document.getElementById("content").style.visibility = false;
 
     var siderPage = document.getElementById("siderPage");
@@ -87,13 +94,12 @@ function content_finder(){
                 ul_in_siderPage[layer].innerHTML = ul_in_siderPage[layer].innerHTML + `<li><ul><li><button type="button" onclick="window.location.href='#${title_in_content[i].innerHTML+"-"+i}';">${title_in_content[i].innerHTML}</button></li></ul></li>`;
                 layer_max++;
                 layer = layer_max;
-            }else if(title_in_content[i].tagName == title_in_content[0].tagName){
-                layer = 0;
-                ul_in_siderPage[layer].innerHTML = ul_in_siderPage[layer].innerHTML + `<li><button type="button" onclick="window.location.href='#${title_in_content[i].innerHTML+"-"+i}';">${title_in_content[i].innerHTML}</button></li>`
             }else{
-                layer += title_in_content[i].tagName.substring(1,2) - title_in_content[i-1].tagName.substring(1,2);
-                ul_in_siderPage[layer].innerHTML = ul_in_siderPage[layer].innerHTML + `<li><button type="button" onclick="window.location.href='#${title_in_content[i].innerHTML+"-"+i}';">${title_in_content[i].innerHTML}</button></li>`
-            }
+				if(title_in_content[i].tagName == title_in_content[0].tagName) layer = 0 
+				else layer += title_in_content[i].tagName.substring(1,2) - title_in_content[i-1].tagName.substring(1,2)
+				
+				ul_in_siderPage[layer].innerHTML = ul_in_siderPage[layer].innerHTML + `<li><button type="button" onclick="window.location.href='#${title_in_content[i].innerHTML+"-"+i}';">${title_in_content[i].innerHTML}</button></li>`
+			} 
         }
     }
 }
--- personal_web/js/note_of_noval_by_skyscar.js
+++ personal_web/js/note_of_noval_by_skyscar.js
@@ -79,4 +79,3 @@ function textReader(){
         alert(fileReader.error);
     }
 }
-
