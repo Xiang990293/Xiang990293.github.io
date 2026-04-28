a1053fba4e90038bb01dd2bf66c088b2e401ce61
Xiang990293 <990293kwi@gmail.com>
2026-04-29 00:34:45 +0800

    fixed internal error v1.7.2

--- router/tools.js
+++ router/tools.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const fs = require('fs').promises;
+const fs = require('fs');
 const i18next = require('i18next');
 
 // 匯出路由
